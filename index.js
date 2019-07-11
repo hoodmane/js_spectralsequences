13a05c69e8392fbc1ddda7899b5026008813c94f
Dexter Chua <dalcde@yahoo.com.hk>
2019-07-11 18:59:07 -0400

    Move status out of display.js

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ function addLoadingMessage(message){
     console.log(message);
 }
 
+function setStatus(html){
+    if(window.status_div_timer){
+        clearTimeout(window.status_div_timer);
+    }
+    document.getElementById("status").innerHTML = html;
+}
+
+function delayedSetStatus(html, delay){
+    window.status_div_timer = setTimeout(() => setStatus(html), delay);
+}
+
 url = new URL(document.location)
 jsFile = url.searchParams.get("sseq");
 if(jsFile){
