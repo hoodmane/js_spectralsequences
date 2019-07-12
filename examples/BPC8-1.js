c4af95cf821ff05f6a7b847e9b2d226d500c8aaf
Dexter Chua <dalcde@yahoo.com.hk>
2019-07-11 21:50:48 -0400

    Fix BPC8-1.js

--- examples/BPC8-1.js
+++ examples/BPC8-1.js
@@ -112,11 +112,7 @@ $('#layout').w2layout({
     ]
 });
 
-$('#main')[0].id = '';
 $('#layout_layout_panel_right').css('border-left', '1px solid silver');
-let el = w2ui['layout'].el('main');
-el.id = "#main";
-
 
 let grid_config = {
     name: 'grid',
@@ -750,7 +746,7 @@ IO.loadFromServer(getJSONFilename("BPC8-1-E13")).then(function(json){
     }
     addLoadingMessage(`Added differentials in ${getTime()} seconds.`);
     document.getElementById("loading").style.display =  "none";
-    sseq.display("#main");
+    sseq.display(w2ui.layout.el('main'));
     // IO.download("BPC8-1.svg", display.toSVG());
     addLoadingMessage(`Displayed in ${getTime()} seconds.`);
     let t1 = performance.now();
@@ -999,7 +995,7 @@ function setupDifferentialInterface(json){
     dss.addEventHandler("ctrl+shift+z", undo.redo);
 
     sseq.updateAll();
-    dss.display("#main");
+    dss.display(w2ui.layout.el('main'));
     setTimeout(() => differential_family.refreshRecords(), 500);
     w2ui.layout.onResize = function onResize(event) {
         event.onComplete = function onResizeComplete(){
