048417ac3d8a9058f64348f819245516f793a9a3
Dexter Chua <dalcde@yahoo.com.hk>
2019-07-29 11:56:30 -0400

    More robust color management in ExportToTex.js

--- src/ExportToTex.js
+++ src/ExportToTex.js
@@ -5,15 +5,20 @@ function SpectralSequenceToTex(sseq, page, xmin, xmax, ymin, ymax){
     let classStrings = [];
     let edgeStrings = [];
     let outputString = [];
+
+    let colormap = new Map();
+    colormap.context = document.createElement("canvas").getContext("2d");
+
     for(let c of classes){
-        classStrings.push(latexClassString(c, page));
+        classStrings.push(latexClassString(c, page, colormap));
     }
 
     for(let edge of edges){
-        edgeStrings.push(latexEdgeString(edge, page));
+        edgeStrings.push(latexEdgeString(edge, page, colormap));
     }
 
     outputString.push(getBeginString(page, xmin, xmax, ymin, ymax));
+    outputString.push(Array.from(colormap, x => `\\definecolor{${x[1][0]}}{RGB}{${x[1][1][0]},${x[1][1][1]},${x[1][1][2]}} % ${x[0]}`).join("\n"));
     outputString.push(classStrings.join("\n"));
     outputString.push(edgeStrings.join("\n"));
     outputString.push("\\end{sseqpage}");
@@ -28,7 +33,7 @@ function DownloadSpectralSequenceTex(filename, sseq, page, xmin, xmax, ymin, yma
 function getBeginString(page, xmin, xmax, ymin, ymax) {
     return `\\begin{sseqpage}[degree = {-1}{#1}, x range = {${xmin}}{${xmax}}, y range = {${ymin}}{${ymax}}]`;
 }
-function latexClassString(c, page){
+function latexClassString(c, page, colormap){
     let options = [];
     options.push(`name=${"id"+c.unique_id}`);
 
@@ -36,19 +41,17 @@ function latexClassString(c, page){
     if (node.fill)
         options.push("fill");
 
-    let color = getTeXColor(node.color);
-    if (color)
-        options.push(color)
+    if (node.color)
+        options.push(getTeXColor(node.color, colormap))
 
     return `\\class[${options.join(", ")}](${c.x},${c.y})`;
 }
 
-function latexEdgeString(edge, page){
+function latexEdgeString(edge, page, colormap){
     let options = [];
 
-    let color = getTeXColor(edge.color);
-    if (color)
-        options.push(color);
+    if (edge.color)
+        options.push(getTeXColor(edge.color, colormap));
 
     switch (edge.type) {
         case "Structline":
@@ -62,10 +65,29 @@ function latexEdgeString(edge, page){
     }
 }
 
-function getTeXColor(color) {
-    if (!color.startsWith("#"))
-        return color;
+function getTeXColor(color, colormap) {
+    if (!colormap.has(color)) {
+        colormap.context.fillStyle = "white";
+        colormap.context.fillRect(0, 0, 1, 1);
+        colormap.context.fillStyle = color;
+        colormap.context.fillRect(0, 0, 1, 1);
+
+        colormap.set(color, [randomString(10), colormap.context.getImageData(0, 0, 1, 1).data]);
+    }
+
+    return colormap.get(color)[0];
 }
 
+function randomChar() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomString(len) {
+    let result = "";
+    for (let i = 0; i < len; i++)
+        result += randomChar();
+
+    return result;
+}
 exports.SpectralSequenceToTex = SpectralSequenceToTex;
 exports.DownloadSpectralSequenceTex = DownloadSpectralSequenceTex;
