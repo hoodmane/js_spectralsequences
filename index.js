6774d64bc69087c711da34ceedc9c0f287c43002
Hood <hood@mit.edu>
2019-09-16 11:39:50 -0400

    Updates to listing.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+url = new URL(document.location)
+jsFile = url.searchParams.get("sseq");
 function addLoadingMessage(message){
     let msg_div = document.getElementById('loading');
     if(msg_div == null){
@@ -14,48 +16,112 @@ function addLoadingMessage(message){
     console.log(message);
 }
 
-url = new URL(document.location)
-jsFile = url.searchParams.get("sseq");
-if(jsFile){
-    document.body.id = "sseq_display";
-    // let mainDiv = document.createElement("div");
-    // mainDiv.id = "main";
-    // document.body.appendChild(mainDiv);
-    let script = document.createElement('script');
-    // script.onload = function () {
-    //     //do stuff with the script
-    // };
-    script.src = jsFile;
-    document.body.appendChild(script);
-} else {
-    while (document.body.firstChild) {
-        document.body.removeChild(document.body.firstChild);
+function setStatus(html){
+    if(window.status_div_timer){
+        clearTimeout(window.status_div_timer);
     }
-    document.body.id = "spectral_sequences";
-    let heading = document.createElement("h1");
-    heading.innerHTML = "Spectral Sequences";
-    document.body.appendChild(heading);
-    let table = document.createElement("table");
-    for(let example of example_list){
-        let tr = document.createElement("tr");
-        let tdA = document.createElement("td");
-        tdA.className = "file";
-        let tdB = document.createElement("td");
-        tdB.className = "description";
-        let a = document.createElement("a");
-        a.href = "?sseq=" + example["filename"];
-        if(example["properties"]["name"]){
-            a.innerHTML = example["properties"]["name"];
-        } else {
-            a.innerHTML = example["filename"];
-        }
-        tdA.appendChild(a);
-        tdB.innerHTML = example["properties"]['description'];
-        tr.appendChild(tdA);
-        if(example["properties"]['description']){
-            tr.appendChild(tdB);
-        }
-        table.appendChild(tr);
-    }
-    document.body.appendChild(table);
+    document.getElementById("status").innerHTML = html;
 }
+
+function delayedSetStatus(html, delay){
+    window.status_div_timer = setTimeout(() => setStatus(html), delay);
+}
+
+macros = {
+    "\\toda" : ["\\langle #1\\rangle",1],
+    "\\tmf" : "\\mathit{tmf}",
+    "\\HF" : "H\\F",
+    "\\HZ" : "H\\Z",
+    "\\semidirect" : "\\rtimes",
+    "\\F" : "\\mathbb{F}",
+    "\\Z" : "\\mathbb{Z}",
+    "\\Zbb" : "\\mathbb{Z}",
+    "\\CP" : "\\mathbb{CP}"
+}
+
+function katexMathInDelims(string){
+    html_list = string.split(/(?:\\\[)|(?:\\\()|(?:\\\))|(?:\\\])|(?:\$)/);
+    for(let i = 1; i < html_list.length; i+=2){
+        html_list[i] = katex.renderToString(html_list[i], {macros : macros});
+    }
+    return html_list.join("");
+}
+
+IO.loadFromServer("spectralsequences_listing.json").then(example_list => {
+    if(jsFile){
+        document.body.id = "sseq_display";
+        let mainDiv = document.createElement("div");
+        mainDiv.id = "main";
+        document.body.appendChild(mainDiv);
+        let jsFilePath;
+        for(let example of example_list){
+            if(example["js"] === jsFile){
+                jsFilePath = `js_spectralsequences/examples/${example["js-file"] || example["js"]}.js`;
+                break;
+            }
+        }
+        
+        let script = document.createElement('script');
+        // script.onload = function () {
+        //     //do stuff with the script
+        // };
+        script.src = jsFilePath;
+        document.body.appendChild(script);
+    } else {
+        document.body.id = "spectral_sequences";
+        document.documentElement.style.overflow = "";
+        let heading = document.createElement("h1");    
+        heading.innerHTML = "Spectral Sequences";   
+        let table = document.createElement("table");
+        document.body.appendChild(heading);
+        document.body.appendChild(table);
+        table.style.marginBottom = "100px";
+        table.style.marginRight = "40px";
+        console.log(example_list);
+        example_list.sort(function(a,b) {return (a["name"] > b["name"]) ? 1 : ((b["name"] > a["name"]) ? -1 : 0);} ); 
+        let colgroup = document.createElement("colgroup");
+        let col = document.createElement("col");
+        col.setAttribute("width",300);
+        colgroup.appendChild(col);
+        table.appendChild(colgroup);
+        let n = -1;
+        for(let example of example_list){
+            console.log(example);
+            n ++;
+            let tr = document.createElement("tr");
+            tr.style.marginBottom = "20pt";
+            let tdA = document.createElement("td");
+            tdA.className = "file";
+            tdA.style.verticalAlign = "top";
+            let tdB = document.createElement("td");
+            tdB.className = "description";
+            let a = document.createElement("a");
+            if(example["js"]){
+                a.href = `?sseq=${example["js"]}`;
+            } else if(example["url"]) {
+                a.href = example["url"];
+            }
+            a.innerHTML = katexMathInDelims(example["name"]);
+            tdA.appendChild(a);
+            if(example["latex"] && a.href){
+                tdA.innerHTML += ` (<a href='latex/${example["latex"]}.pdf'>pdf</a>, <a href='latex/${example["latex"]}.tex'>source</a>)`;
+            } else if(example["latex"] && ! a.href){
+                a.href = `latex/${example["latex"]}.pdf`;
+                tdA.innerHTML += ` (<a href='latex/${example["latex"]}.tex'>source</a>)`;
+            }
+            tr.appendChild(tdA);
+            if(example["description"]){
+                tdB.innerHTML = katexMathInDelims(example["description"]);
+                tr.appendChild(tdB);
+                tr.id = "row" + n;
+                table.appendChild(tr);
+            } else {
+                console.log(example["name"]);
+            }
+            // MathJax.Hub.Queue(["Typeset", MathJax.Hub, "row" + n]);
+            // MathJax.Hub.Queue(() => {
+            //     tr.removeAttribute("style");
+            // });
+        }        
+    }
+});
